817f5ded5a5d590ce9e45ecbb6711c52f58eb425
Lexys <73967017+LexyysS@users.noreply.github.com>
2025-03-29 01:18:34 -0300

    Update ConfiguracionSeguridad.java

--- src/main/java/com/nataliapena/seguridad/ConfiguracionSeguridad.java
+++ src/main/java/com/nataliapena/seguridad/ConfiguracionSeguridad.java
@@ -55,7 +55,7 @@ public class ConfiguracionSeguridad {
 	    @Bean
 	    public CorsConfigurationSource corsConfigurationSource() {
 	        CorsConfiguration config = new CorsConfiguration();
-	        config.setAllowedOrigins(List.of("http://truequemundos.netlify.app"));
+	        config.setAllowedOrigins(List.of("https://truequemundos.netlify.app"));
 	        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 	        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 	        config.setAllowCredentials(true);
